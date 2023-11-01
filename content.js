f96e5e67eb134c57c75fe42f35d3ac0275ae97d4
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-11-01 14:29:02 -0700

    color changer test
    added color changing for text and sidebar

--- content.js
+++ content.js
@@ -1,6 +1,21 @@
 window.onmouseover = function() {
+    // Background image
     chrome.storage.local.get('img', function (result) {
-        document.querySelector(".monaco-editor-background").style.backgroundImage
-            = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("' + result.img + '")';
+        if (result.img) {
+            document.querySelector(".monaco-editor-background").style.backgroundImage
+                = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("' + result.img + '")';
+        } else {
+            document.querySelector(".monaco-editor-background").style.backgroundImage = "";
+        }
+    });
+
+    // Sidebar color
+    chrome.storage.local.get('sidebar', function (result) {
+        document.documentElement.style.setProperty("--theia-sideBar-background", result.sidebar);
+    });
+
+    // Text color
+    chrome.storage.local.get('text', function (result) {
+        document.documentElement.style.setProperty("--theia-foreground", result.text);
     });
 }
--- popup.js
+++ popup.js
@@ -1,18 +1,45 @@
 window.onload = function() {
-    var img;
+    var img, sidebar, text;
     chrome.storage.local.get('img', function (result) {
         if (result.img){
 		    document.getElementById("in").value = result.img;
         }
     });
 
+    chrome.storage.local.get('sidebar', function (result) {
+        if (result.sidebar){
+		    document.getElementById("in-sidebar").value = result.sidebar;
+        } else {
+            document.getElementById("in-sidebar").value = "#252526";
+        }
+    });
+
+    chrome.storage.local.get('text', function (result) {
+        if (result.text){
+		    document.getElementById("in-text").value = result.text;
+        } else {
+            document.getElementById("in-text").value = "#cccccc";
+        }
+    });
+
     document.getElementById("save").onclick = function fun() {
-		img = document.getElementById("in").value;
-		chrome.storage.local.set({'img': img});
+        img = document.getElementById("in").value;
+        sidebar = document.getElementById("in-sidebar").value;
+        text = document.getElementById("in-text").value;
+
+        chrome.storage.local.set({'img': img});
+        chrome.storage.local.set({'sidebar': sidebar});
+        chrome.storage.local.set({'text': text});
     }
 
     document.getElementById("reset").onclick = function fun() {
 		document.getElementById("in").value = "";
 		chrome.storage.local.set({'img': null});
+
+        document.getElementById("in-sidebar").value = "#252526";
+		chrome.storage.local.set({'sidebar': "#252526"});
+
+        document.getElementById("in-text").value = "#cccccc";
+		chrome.storage.local.set({'text': "#cccccc"});
     }
 }
